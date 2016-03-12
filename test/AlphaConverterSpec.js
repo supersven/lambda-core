4ac52f477613ef495979946a763360ae1ea241d5
Sven Tennie <sven.tennie@gmail.com>
2016-03-12 18:53:17 +0100

    add AlphaConverter

---
+++ test/AlphaConverterSpec.js
@@ -0,0 +1,50 @@
+"use strict";
+
+var expect = require('expect.js');
+var AlphaConverter = require('../lib/AlphaConverter').alphaConverter;
+var Printer = require('../lib/PrettyPrinter').printer;
+var Parser = require('../lib/Parser').parser;
+
+describe("AlphaConverter", function () {
+
+    function convert(expression) {
+        var converted = AlphaConverter.convert(Parser.parse(expression).get("value"));
+        return Printer.print(converted);
+    }
+
+    it("should not change '\\x->x'", function () {
+        var result = convert("\\x->x");
+
+        expect(result).to.eql('\\x->x');
+    });
+
+    it("should not change '\\x->1'", function () {
+        var result = convert("\\x->1");
+
+        expect(result).to.eql('\\x->1');
+    });
+
+    it("should alpha convert '\\x->\\x->x' to '\\x->\\x1->x1'", function () {
+        var result = convert("\\x->\\x->x");
+
+        expect(result).to.eql('\\x->\\x1->x1');
+    });
+
+    it("should alpha convert '\\x->\\x->\\x->x' to '\\x->\\x1->\\x2->x2'", function () {
+        var result = convert("\\x->\\x->\\x->x");
+
+        expect(result).to.eql('\\x->\\x1->\\x2->x2');
+    });
+
+    it("should alpha convert '(\\x->x)*(\\x->x)' to '(\\x->x)*(\\x1->x1)'", function () {
+        var result = convert("(\\x->x)*(\\x->x)");
+
+        expect(result).to.eql('(\\x->x)*(\\x1->x1)');
+    });
+
+    it("should alpha convert '\\x->(\\x->x)*(\\x->x)' to '\\x->(\\x1->x1)*(\\x2->x2)'", function () {
+        var result = convert("\\x->(\\x->x)*(\\x->x)");
+
+        expect(result).to.eql('\\x->(\\x1->x1)*(\\x2->x2)');
+    });
+});
